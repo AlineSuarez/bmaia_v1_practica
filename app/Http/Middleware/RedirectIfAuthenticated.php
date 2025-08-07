ba82e30252bf63f09c619a60e279edbb891074f8
Paaanciitoo <nicolas.machuca07@inacapmail.cl>
2025-08-06 22:04:57 -0400

    Corrección con los pagos

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -13,7 +13,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect('/dashboard');
+                return redirect('/home');
             }
         }
 
