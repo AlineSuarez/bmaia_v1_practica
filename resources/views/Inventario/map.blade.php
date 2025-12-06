e9aeb5d7888243d1b0d99537af97969579f07af6
Fletopo <fl3t0p0@gmail.com>
2025-12-05 21:27:41 -0300

    se elimino la credencial

--- resources/views/Inventario/map.blade.php
+++ resources/views/Inventario/map.blade.php
@@ -57,7 +57,7 @@
 <!-- Cargar Google Maps API con Places (New) -->
 <script>
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.googleapis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-({key: "AIzaSyA2NAo5G51L09_AtGpxxFZ5V-ejD7iDysw", v: "weekly"});
+({key: "", v: "weekly"});
 </script>
 
 <script>
